d25918f3ee25b5bf7cfab0bea3f0f63e7eb1d9b5
Tim Golden <mail@timgolden.me.uk>
2014-09-25 17:21:26 +0100

    Added some helpful comments to v3

--- GeneratePassword/generate_password_v3.py
+++ GeneratePassword/generate_password_v3.py
@@ -14,6 +14,17 @@ def get_words_from_file(filepath):
     long from within a named file.
     """
     with open(filepath) as f:
+        #
+        # re.findall returns a list of all instances matching a pattern
+        # The pattern \w{3,} matches a word at least three letters long
+        # f.read() reads the entire contents of a file into a string
+        # set() returns a unique set of the values in an iterator
+        #
+        # So we have:
+        #   A unique set of
+        #     All words at least three letters long within
+        #       The contents of the file f
+        #
         return set(re.findall(r"\w{3,}", f.read()))
 
 def generate(filename, password_length, number_of_words):
@@ -21,10 +32,29 @@ def generate(filename, password_length, number_of_words):
     as long as password_length.
     """
     words = get_words_from_file(filename)
+    #
+    # We need enough letters to produce a word at least as long as
+    # password length. We know how many words to use, so we do some
+    # simple division-and-remainder maths to get the number of letters
+    # long which they must be.
+    #
     quotient, remainder = divmod(password_length, number_of_words)
     word_length = quotient + (1 if remainder else 0)
+    #
+    # Copy all words of the right length into another list
+    # and shuffle it to avoid generating the same password several
+    # times.
+    #
     suitable_words = list(w for w in words if len(w) == word_length)
     random.shuffle(suitable_words)
+    #
+    # Then return the right number of words, each starting with a
+    # capital letter, joined together.
+    #
+    # list[:number] gives the first number of words
+    # word.title() gives a version of the word with an initial capital
+    # "".join(... list of words ...) returns all the words joined up
+    #
     return "".join(w.title() for w in suitable_words[:number_of_words])
 
 if __name__ == '__main__':
